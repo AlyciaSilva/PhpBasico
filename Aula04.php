4911231d6a9bee8d1b29a9e6c40ba064656a686c
AlyciaDev <alyciasilvaprogrammer@gmail.com>
2020-10-02 23:48:30 -0300

    first commit

---
+++ Aula04.php
@@ -0,0 +1,13 @@
+<doctype! html>
+<html>
+    <head>
+        <meta charset = "utf-8"/>
+    </head>
+    <body>
+        <?php
+        $n = 4;
+        $nome = Gustavo;
+        echo $nome; 
+        ?>
+    </body>
+<\html>
