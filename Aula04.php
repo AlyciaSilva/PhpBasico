78cf42a59083f26296b7af23206baa95cc5ab163
AlyciaDev <alyciasilvaprogrammer@gmail.com>
2020-10-06 21:37:27 -0300

    Meu cod

--- Aula04.php
+++ Aula04.php
@@ -1,13 +1,22 @@
-<doctype! html>
 <html>
     <head>
         <meta charset = "utf-8"/>
+        <link rel = "stylesheet" href = "_css4/estilo.css"/>
     </head>
     <body>
-        <?php
-        $n = 4;
-        $nome = Gustavo;
-        echo $nome; 
-        ?>
+       <div>
+            <?php
+                $n = 4;
+                $nome = "Gustavo ";
+                echo $nome;
+                #concatenando variaveis 
+                $idade = 36;
+                echo $nome." tem ".$idade." anos."; 
+                #ou tbm
+                echo "\n $nome tem $idade anos!!";
+                #Mas usando as aspas simples '', não há diferenciação
+                echo '\n $nome tem $idade anos!!';
+            ?>
+       </div> 
     </body>
-<\html>
+</html>
