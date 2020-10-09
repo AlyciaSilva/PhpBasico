c4aa2654a41f14ff1557e817b06aee1ebd390797
AlyciaDev <alyciasilvaprogrammer@gmail.com>
2020-10-08 22:57:56 -0300

    funções

--- Aula05.php
+++ Aula05.php
@@ -2,6 +2,11 @@
     <head>
         <meta charset = "utf-8"/>
         <link rel = "stylesheet" href = "_css4/estilo.css"/>
+        <style>
+        h2{ font: 15pt Arial;
+        color: #171559;
+        }
+        </style>
     </head>
     <body>
        <div>
@@ -30,6 +35,15 @@
                 $media1 = ($a+$b)/2;
                 echo "<br/>A media é $media";
                 #Funções aritiméticas
+                $v1 = $_GET['x']
+                $v2= $_GET['y']
+                echo "<h2>Os valores recebidos: $v1 e $v2</h2>";
+                echo "O valor absoluto de $v2 e".abs($v2); #Se o valor for negativo transforma em positivo
+                echo "<br/>o valor de $v1<sup>$v2</sup> e".pow($v1,$v2);
+                echo "<br/>A raiz de $v1 e".sqrt($v1);
+                echo "<br/>O arredondamento de $v2 e".round($v2); #usando ceil arredonda para cima e floor para baixo
+                echo "<br/>A parte inteira de $v2 e".intval($v2);
+                echo "<br/> O valor de $v1 em moeda é R$".sqrt($v1,2);
             ?>
        </div> 
     </body>
