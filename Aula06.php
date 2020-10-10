1fcc155045ffeb9a4fc83c967afaaa44c97e28f3
AlyciaDev <alyciasilvaprogrammer@gmail.com>
2020-10-09 21:05:05 -0300

    completo

--- Aula06.php
+++ Aula06.php
@@ -6,12 +6,26 @@
     <body>
        <div>
             <?php
-                $preco = $_GET['a']
-                echo "O preço é de".number_format($preco, 2)
+                $preco = $_GET['a'];
+                echo "O preço é de ".number_format($preco, 2);
                 #$desconto = ($preco*10)/100 (como fiz)
                 //$preco += ($preco*10)/100 // aumento
-                $preco -= ($preco*10)/100 //desconto
-                echo "<br/>O preço final é de".number_format($preco, 2)//.($desconto + $preco) 
+                $preco -= ($preco*10)/100; //desconto
+                echo "<br/>O preço final é de".number_format($preco, 2);//.($desconto + $preco) 
+                #Decremento
+                $atual = $_GET["aa"];
+                echo "<br/>O ano atual e $atual e o ano anterior é ".(--$atual);
+                #Uso de ariaveis referenciadas
+                $a = 3;
+                $b = &$a;
+                $b += 5;
+                echo "<br/>A variavel A vale $a";
+                echo "<br/>A variavel B vale $b";
+                #Variaveis de variaveis
+                $x = "abc";
+                $$x = "def";
+                echo "<br/>O conteudo da varaivel X é $x";
+                echo "<br/>A variavel criada recebeu o valor $abc";
             ?>
        </div> 
     </body>
